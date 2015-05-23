d60e83f81914f30dbbce01d42bcf9cb0889b202c
Shayne Chow <shayne@authbuy.org>
2015-05-23 18:00:35 +0800

    构建项目基础框架：TabBar 标签栏 + Navigation 导航栏

---
+++ ZXBlurSideBar/Controllers/secondPageViewController.m
@@ -0,0 +1,46 @@
+//
+//  secondPageViewController.m
+//  StartUpDemo
+//
+//  Created by ChowShayne on 15/3/9.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import "secondPageViewController.h"
+
+@interface secondPageViewController ()
+
+@end
+
+@implementation secondPageViewController
+
+#pragma mark - ViewController-life
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"Opinion";
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ ZXBlurSideBar/Controllers/thirdPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  thirdPageViewController.h
+//  StartUpDemo
+//
+//  Created by ChowShayne on 15/3/9.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface thirdPageViewController : UIViewController
+
+@end
