b23777ea611dac6ab7fcdbdc6c8115dae18314a7
Shayne Chow <shayne@authbuy.org>
2015-05-23 16:56:40 +0800

    Initial Commit

---
+++ ZXBlurSideBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXBlurSideBar
+//
+//  Created by ChowShayne on 15/5/23.
+//  Copyright (c) 2015年 微加科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
